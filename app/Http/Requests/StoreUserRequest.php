154a75872f4d659602b12c551e9412120dd97674
unknown <omidasgary966@gmail.com>
2024-07-21 08:34:47 +0330

    edit validation store and update email

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -27,7 +27,7 @@ class StoreUserRequest extends FormRequest
             'phone_number' => 'required|string|regex:/^\+?[0-9]{10,15}$/',
             'date_of_birth' => 'required|date|before:today',
             'national_code' => 'required|string|size:10|regex:/^[0-9]{10}$/',
-            'email' => 'required|string|max:255|unique:users',
+            'email' => 'string|max:255|unique:users|nullable',
             'password' => 'required|string|min:8',
         ];
     }
