d31b0b0584026aa2f5389d9399b4fb074862929f
unknown <omidasgary966@gmail.com>
2024-07-21 15:58:06 +0330

    creation index function for ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
 }
