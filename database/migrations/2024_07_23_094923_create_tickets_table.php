2349c01f06cfedd53010854908a36c4329756178
unknown <omidasgary966@gmail.com>
2024-07-23 14:21:20 +0330

    edit migration and model name for ticket

--- database/migrations/2024_07_23_094923_create_tickets_table.php
+++ database/migrations/2024_07_23_094923_create_tickets_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tikets', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('title');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tikets');
+        Schema::dropIfExists('tickets');
     }
 };
