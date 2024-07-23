cd4d49500c968e3deb02f98ac2f3e8a76c8eec92
unknown <omidasgary966@gmail.com>
2024-07-23 14:25:00 +0330

    crateion index function for TicketController

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function index()
+    {
+        $tickets = Ticket::all();
+        return response()->json($tickets);
+    }
 }
