e6ea66e481585de0ed1db0ebde2137e8ae6d8faa
unknown <omidasgary966@gmail.com>
2024-07-25 09:53:49 +0330

    add store function for RuleController

--- app/Http/Controllers/RuleController.php
+++ app/Http/Controllers/RuleController.php
@@ -13,4 +13,10 @@ class RuleController extends Controller
         $rules = Rule::all();
         return response()->json(['rules' => 'rules']);
     }
+
+    public function store(Request $request)
+    {
+      $rule = Rule::create($request->all());
+      return response()->json(['message' => 'قانون با موفقیت ایجاد شد','rule' => $rule],201);
+    }
 }
