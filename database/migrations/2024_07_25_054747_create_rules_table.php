33b2e9b40b5277c4fceb5c9dbbe7c608a7647d78
unknown <omidasgary966@gmail.com>
2024-07-25 15:35:40 +0330

    add softDelete column To Rule Migration

--- database/migrations/2024_07_25_054747_create_rules_table.php
+++ database/migrations/2024_07_25_054747_create_rules_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('rules', function (Blueprint $table) {
             $table->id();
             $table->string('body');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
