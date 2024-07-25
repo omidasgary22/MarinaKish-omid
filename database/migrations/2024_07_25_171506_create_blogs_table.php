13ea81a151b729fdfbb8d6ca31f9029d9da5598f
unknown <omidasgary966@gmail.com>
2024-07-25 20:50:22 +0330

    creation blogs migration

---
+++ database/migrations/2024_07_25_171506_create_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary');    //توضیح مختصر
+            $table->text('content');    //جزییات مطالب
+            $table->integer('duration_of_study');    //مدت زمان مطالعه
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
