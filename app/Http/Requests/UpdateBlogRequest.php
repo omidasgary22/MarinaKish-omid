41299125905d428a22c14321e9e4707d4c544295
unknown <omidasgary966@gmail.com>
2024-07-26 15:22:58 +0330

    creation UpdateBlogRequest

---
+++ app/Http/Requests/UpdateBlogRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBlogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'summary' => 'somtimes|required|string|max:500',
+            'content' => 'sometimes|required|string',
+            'duration_of_study' => 'sometimes|required|integer|min:1',
+        ];
+    }
+}
