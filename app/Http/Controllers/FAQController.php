5e947ac67fcf40ae245ff3eb5cb48f576ec073ad
unknown <omidasgary966@gmail.com>
2024-07-29 10:29:47 +0330

    add Store function To FAQ

--- app/Http/Controllers/FAQController.php
+++ app/Http/Controllers/FAQController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreFAQRequest;
 use App\Models\FAQ;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,11 @@ class FAQController extends Controller
     {
         $faqs = FAQ::all();
         return response()->json(['faqs' => $faqs]);
+    }
+
+    public  function store(StoreFAQRequest $request)
+    {
+        $faq = FAQ::create($request->toArray());
+        return response()->json(['message' => 'سوال با موفقیت ایجاد شد', 'faq' => $faq], 201);
     }
 }
