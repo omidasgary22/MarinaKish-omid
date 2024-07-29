a1b8620da1af82166eae1d2a59d7d5049b291ab4
unknown <omidasgary966@gmail.com>
2024-07-29 09:26:37 +0330

    creation FAQ migration & add index function

---
+++ app/Http/Controllers/FAQController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\FAQ;
+use Illuminate\Http\Request;
+
+class FAQController extends Controller
+{
+    public function index()
+    {
+        $faqs = FAQ::all();
+        return response()->json(['faqs' => $faqs]);
+    }
+}
