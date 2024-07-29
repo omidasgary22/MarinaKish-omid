34b084fa957730bc1ce89ddc6c3f039e4a0dbbd5
unknown <omidasgary966@gmail.com>
2024-07-29 08:48:45 +0330

    creation FAQ model & casts

---
+++ app/Models/FAQ.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FAQ extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillab = [
+        'question',
+        'answer'
+    ];
+
+
+    protected $casts = [
+        'question' => 'string',
+        'answer' => 'string',
+    ]
+}
