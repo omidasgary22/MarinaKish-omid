eecb56b12278de46b3ad6e8d45da1048ca3452f7
unknown <omidasgary966@gmail.com>
2024-07-31 12:09:04 +0330

    creation StoreCommentRequest

---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'product_id' => 'required|exists:products,id',
+            'title' => 'required|string|max:300',
+            'body' => 'required|string',
+            'answer' => 'nullable|string',
+            'star' => 'required|in:1,2,3,4,5',
+            'status' => 'required|in:pending,approved,rejected',
+        ];
+    }
+}
