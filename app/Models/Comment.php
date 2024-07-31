20be97cdaae5654823252962294fb0485cbc7e49
unknown <omidasgary966@gmail.com>
2024-07-31 09:24:53 +0330

    creation model comment &filable

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'title',
+        'body',
+        'answer',
+        'star',
+        'status'
+    ];
 }
