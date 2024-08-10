17a73fe28f68bf2f4b3069fbbd71c4123eb5e95f
Omid <omidasgary966@gmail.com>
2024-08-10 19:52:59 +0330

    edit change password and give permission to password change comtroller

--- app/Http/Controllers/PasswordChangeController.php
+++ app/Http/Controllers/PasswordChangeController.php
@@ -14,15 +14,20 @@ class PasswordChangeController extends Controller
 {
     public function update(ChangePasswordRequest $request): JsonResponse
     {
-        if (!Hash::check($request->current_password, Auth::user()->password)) {
-            return response()->json(['message' => 'رمز عبور فعلی اشتباه است.'], 400);
+        $user = $request->user();
+        if ($request->user()->can('PasswordChange')) {
+            if (!Hash::check($request->current_password, $user->password)) {
+                return response()->json(['message' => 'رمز عبور فعلی اشتباه است.'], 400);
+            }
+
+
+            $user->update([
+                'password' => Hash::make($request->new_password),
+            ]);
+
+            return response()->json(['message' => 'رمز عیور با موفقیت تغییر یافت.']);
+        } else {
+            return response()->json(['message' => 'شما دستسی لازم برای انجام این کار را ندارید'], 403);
         }
-
-
-        Auth::user()->update([
-            'password' => Hash::make($request->new_password),
-        ]);
-
-        return response()->json(['message' => 'رمز عیور با موفقیت تغییر یافت.']);
     }
 }
