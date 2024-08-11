ac35c250ff9b0bdfe762ab7c405c0ff5930a30b6
Omid <omidasgary966@gmail.com>
2024-08-11 15:46:25 +0330

    creation sans model

--- app/Models/Sans.php
+++ app/Models/Sans.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Sans extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'start_time',
+        'capacity',
+    ];
+
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
