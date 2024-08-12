3e84389030f83e83c0391ee42b1f065924e61b79
Omid <omidasgary966@gmail.com>
2024-08-12 14:31:35 +0330

    add validation for reservation controller and add stor function to reservation controller

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationController extends Controller
 {
@@ -13,8 +14,29 @@ class ReservationController extends Controller
             ->where('reseravetion_date', $reservation_date)
             ->first();
 
-            return $existingReservation == null;
+        return $existingReservation == null;
     }
 
-    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'sans_id' => 'required|exists:sans,id',
+            'reservation_date' => 'required|date',
+        ]);
+
+        $sans_id = $request->input('sans_id');
+        $reservation_date = $request->input('reservation_date');
+
+        if (!$this->checkAvailability($sans_id, $reservation_date)) {
+            return response()->json(['message' => 'این سانس در این تاریخ قبلا رزرو شده است'], 400);
+        }
+
+        $reservation = Reservation::create([
+            'user_id' => Auth::id(),
+            'sans_id' => $sans_id,
+            'reservation_date' => $reservation_date,
+        ]);
+
+        return response()->json(['message' => 'رزرو با موفقیت انجام شد.', 'reservation' => $reservation], 201);
+    }
 }
