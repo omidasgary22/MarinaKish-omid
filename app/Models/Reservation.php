41778f9324641b4ce8be7cd2f641d1c5921f2bda
Omid <omidasgary966@gmail.com>
2024-08-12 13:44:00 +0330

    add user and sans realation in reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sans()
+    {
+        return $this->belongsTo(Sans::class);
+    }
 }
