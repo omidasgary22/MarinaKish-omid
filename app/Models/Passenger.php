1faf7e345e2099d04b5c3f68cc102f19200874e1
Omid <omidasgary966@gmail.com>
2024-08-14 13:46:16 +0330

    aggfillable in passenger model

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Passenger extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    protected $fillable = [
+        'user_id',
+        'Name and surname',
+        'gender',
+        'age',
+        'national_code',
+    ]
 }
