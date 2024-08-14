3e3ceae9ad7aebb7bc5b414b2db31410feab2aa3
Omid <omidasgary966@gmail.com>
2024-08-14 13:39:22 +0330

    creation passenger migration whit column

---
+++ database/migrations/2024_08_14_100213_create_passengers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('Name and surname');
+            $table->enum('gender', ['زن', 'مرد']);
+            $table->integer('age');
+            $table->string('national_code')->unique();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passengers');
+    }
+};
