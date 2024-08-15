e61d5aa4d077f12316bab8c0f7a997fa1639e266
Omid <omidasgary966@gmail.com>
2024-08-15 14:15:16 +0330

    creation discount_code migration

---
+++ database/migrations/2024_08_15_103659_create_discount_codes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('code');
+            $table->decimal('discount_percentage', 5, 2);
+            $table->integer('quantity');
+            $table->timestamps('start_at');
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_codes');
+    }
+};
