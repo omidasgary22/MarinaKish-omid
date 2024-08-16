bf8ee92a51a8ada9db8e41256a5253a5a2d428d0
Omid <omidasgary966@gmail.com>
2024-08-16 12:40:00 +0330

    creation manage use discount_code

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DiscountCode;
 use App\Models\Passenger;
 use App\Models\Product;
 use App\Models\Reservation;
@@ -17,7 +18,7 @@ class ReservationController extends Controller
             ->where('reservation_date', $reservation_date)
             ->doesntExist();
     }
-    
+
 
     public function store(Request $request)
     {
@@ -33,7 +34,7 @@ class ReservationController extends Controller
         $reservation_date = $validatedData['reservation_date'];
         $product_id = $validatedData['product_id'];
         $passengerIds = $request->input('passengers', []);
-    
+
 
         $user = Auth::user();
         $sans = Sans::findOrFail($sans_id);
@@ -82,5 +83,39 @@ class ReservationController extends Controller
         ]);
 
         return response()->json(['message' => 'رزرو با موفقیت انجام شد.', 'reservation' => $reservation, 'total_amount' => $totalAmount,], 201);
+    }
+
+    //اعمال کد تخفیف در زمان پرداخت
+    public function aaplyDiscountCode(Request $request)
+    {
+        $request->validate([
+            'discount_code' => 'required|string|exists:discount_codes,code',
+        ]);
+
+        $discountCode = DiscountCode::where('code', $request->input('discount_code'))->firstOrFail();
+
+        //بررسی تاریخ انتقضا
+        if ($discountCode->expires_at < now()) {
+            return response()->json(['message' => 'کد تخفیف منقضی شده است'], 400);
+        }
+
+        //بررسی استفاده قبلی توسط کاربر
+        $userHasUsedCode = Reservation::where('user_id', Auth::id())
+            ->where('discoun_codedddde_id', $discountCode->id)->exissts();
+
+        if ($userHasUsedCode) {
+            return response()->json(['message' => 'شما قبلا از این کد تخفیف استفاده کرده اید'], 400);
+        }
+
+        //محاسبه مبلغ نهایی با تخفیف
+        $totalAmount = $request->input('total_amount');
+        $discountAmount = ($totalAmount * $discountCode->discount_percentage) / 100;
+        $finalAmount = $totalAmount - $discountAmount;
+
+        return response()->json([
+            'total_amount' => $totalAmount,
+            'discount_amount' => $discountAmount,
+            'final_amount' => $finalAmount,
+        ]);
     }
 }
