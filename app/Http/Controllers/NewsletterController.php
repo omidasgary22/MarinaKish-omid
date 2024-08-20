dcb92d67ccd82ad4ab033f93e1f78abc87e20c12
Omid <omidasgary966@gmail.com>
2024-08-21 00:16:01 +0330

    add permission to newslatter method in controller

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -9,16 +9,24 @@ use Illuminate\Http\Request;
 class NewsletterController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $newsletter = Newsletter::all();
-        return response()->json($newsletter);
+        if ($request->user()->can('news.index')) {
+            $newsletter = Newsletter::all();
+            return response()->json($newsletter);
+        } else {
+            return response()->json(['message' => 'شما دسترسی مجاز زا ندارید']);
+        }
     }
 
-    
+
     public function store(StoreNewsletterRequest $request)
     {
-        $newsletter = Newsletter::create($request->toArray());
-        return response()->json(['success', 'ایمیل شما با موفقیت ثبت شد.']);
+        if ($request->user()->can('news.store')) {
+            $newsletter = Newsletter::create($request->toArray());
+            return response()->json(['success', 'ایمیل شما با موفقیت ثبت شد.']);
+        } else {
+            return response()->json(['message' => 'شما دسترسی مجاز را ندارید']);
+        }
     }
 }
