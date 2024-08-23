f3b593c49ee5a5a00c7a717bc453fbe754fb484c
Omid <omidasgary966@gmail.com>
2024-08-23 16:51:42 +0330

    add comment and edit some Route & Optimizing all Route

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,27 @@
 <?php
 
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\DiscountCodeController;
-use App\Http\Controllers\EmergencyContactController;
-use App\Http\Controllers\FAQController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\NewsletterController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\PassengerController;
-use App\Http\Controllers\PasswordChangeController;
-use App\Http\Controllers\PasswordResetController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\ReportController;
-use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\RuleController;
-use App\Http\Controllers\TicketController;
-use App\Http\Controllers\TickettController;
-use App\Http\Controllers\UserController;
-use App\Models\Blog;
-use App\Models\Newsletter;
-use App\Models\Reservation;
-use GuzzleHttp\Middleware;
-use Illuminate\Foundation\Console\RouteCacheCommand;
-use Illuminate\Http\Request;
+use App\Http\Controllers\{
+    BlogController,
+    CommentController,
+    DiscountCodeController,
+    EmergencyContactController,
+    FAQController,
+    LoginController,
+    LogoutController,
+    NewsletterController,
+    OrderController,
+    PassengerController,
+    PasswordChangeController,
+    PasswordResetController,
+    ProductController,
+    RegisterController,
+    ReportController,
+    ReservationController,
+    RuleController,
+    TicketController,
+    TickettController,
+    UserController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -39,154 +35,148 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//RegisterUser
-Route::post('/register', [RegisterController::class, 'Register'])->name('user.register');
+// Public Routes
 
-//LoginUser
-Route::post('/login', [LoginController::class, 'Login'])->name('user.login');
+// User Registration
+Route::post('/register', [RegisterController::class, 'register'])->name('user.register');
 
+// User Login
+Route::post('/login', [LoginController::class, 'login'])->name('user.login');
 
-
-//User Route
-Route::prefix('users')->middleware('auth:sanctum')->group(function () {
-    Route::get('/index/{id?}', [UserController::class, 'index'])->name('users.index');
-    Route::put('/update/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::patch('/restore/{id}', [UserController::class, 'restore'])->name('users.restore');
-    Route::get('/me', [UserController::class, 'me'])->name('user.me')->name('me');
-    Route::post('/uploade/profile/{id}', [UserController::class, 'uploadProfileFile']);
-    //  Route::post('/store', [UserController::class, 'store'])->withoutmiddleware("auth:sanctum")->name('users.store');
-    // Route::get('/show/{id}', [UserController::class, 'show'])->name('users.show');
-});
-
-//Logout Route
-Route::middleware('auth:sanctum')->delete('/logout', [LogoutController::class, 'logout'])->name('logout');
-
-//ProductRoute
-Route::middleware('auth:sanctum')->prefix('products')->group(function () {
-    Route::get('/index', [ProductController::class, 'index'])->name('products.index');
-    Route::post('/store', [ProductController::class, 'store'])->name('Products.store');
-    Route::put('/update/{id}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-    Route::post('/restore/{id}', [ProductController::class, 'restore'])->name('products.restore');
-    Route::post('/upload/{id}', [ProductController::class, 'uploadImage']);
-});
-
-//TicketRoute
-Route::middleware('auth:sanctum')->prefix('tickets')->group(function () {
-    Route::get('/index', [TicketController::class, 'index'])->name('tickets.index');
-    Route::post('/store', [TicketController::class, 'store'])->name('tickets.store');
-    Route::put('/update/{id}', [TicketController::class, 'update'])->name('tickets.update');
-    Route::delete('/delete/{id}', [TicketController::class, 'destroy'])->name('tikets.destroy');
-    Route::post('/restore/{id}', [TicketController::class, 'restore'])->name('tickets.restore');
-    Route::post('/response/{id}', [TicketController::class, 'addResponse'])->name('tickets.addResponse');
-});
-
-//RuleRoute
-Route::prefix('rules')->group(function () {
-    Route::get('/index/{id?}', [RuleController::class, 'index'])->name('rules.index');
-    Route::post('/store', [RuleController::class, 'store'])->name('rules.store');
-    Route::put('/update/{id}', [RuleController::class, 'update'])->name('rules.update');
-    Route::delete('/delete/{id}', [RuleController::class, 'destroy'])->name('rules.destroy');
-    Route::post('/restore/{id}', [RuleController::class, 'restore'])->name('rules.restore');
-});
-
-//BlogRoute
-Route::prefix('blogs')->group(function () {
-    Route::get('/index', [BlogController::class, 'index'])->name('blogs.index');
-    Route::post('/store', [BlogController::class, 'store'])->name('blogs.store');
-    Route::get('/show/{id}', [BlogController::class, 'show'])->name('blogs.show');
-    Route::put('/update/{id}', [BlogController::class, 'update'])->name('blogs.update');
-    Route::delete('/delete/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
-    Route::get('/restore/{id}', [BlogController::class, 'restore'])->name('blogs.restore');
-    Route::post('/upload/image/{id}', [BlogController::class, 'uploadImage']);
-    //Total removal from the database Route
-    Route::delete('/forcedelete/{id}', [BlogController::class, 'forcedelete'])->name('blog.forceDelete');
-});
-
-//FAQRoute
-Route::middleware('auth:sanctum')->prefix('faqs')->group(function () {
-    Route::get('/index/{id?}', [FaqController::class, 'index'])->name('faqs.index');
-    Route::post('/store', [FaqController::class, 'store'])->name('faqs.store');
-    Route::put('/update/{id}', [FaqController::class, 'update'])->name('faqs.update');
-    Route::delete('/delete/{id}', [FaqController::class, 'destroy'])->name('faqs.destroy');
-    Route::post('/restore/{id}', [FaqController::class, 'restore'])->name('faqs.restore');
-});
-
-//Forget_passwordRoute
+// Password Reset
 Route::post('forgetpassword', [PasswordResetController::class, 'sendResetToken']);
 
-//PasswordChangeRoute
+// Newsletter
+Route::post('/newsletter/store', [NewsletterController::class, 'store'])->name('newsletter.store');
+
+// Routes requiring authentication
+
 Route::middleware('auth:sanctum')->group(function () {
+    // Logout
+    Route::delete('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+    // User Routes
+    Route::prefix('users')->group(function () {
+        Route::get('/index/{id?}', [UserController::class, 'index'])->name('users.index');
+        Route::put('/update/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::patch('/restore/{id}', [UserController::class, 'restore'])->name('users.restore');
+        Route::get('/me', [UserController::class, 'me'])->name('user.me');
+        Route::post('/upload/profile/{id}', [UserController::class, 'uploadProfileFile'])->name('users.uploadProfile');
+    });
+
+    // Product Routes
+    Route::prefix('products')->group(function () {
+        Route::get('/index', [ProductController::class, 'index'])->name('products.index');
+        Route::post('/store', [ProductController::class, 'store'])->name('products.store');
+        Route::put('/update/{id}', [ProductController::class, 'update'])->name('products.update');
+        Route::delete('/delete/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+        Route::post('/restore/{id}', [ProductController::class, 'restore'])->name('products.restore');
+        Route::post('/upload/{id}', [ProductController::class, 'uploadImage'])->name('products.uploadImage');
+    });
+
+    // Ticket Routes
+    Route::prefix('tickets')->group(function () {
+        Route::get('/index', [TicketController::class, 'index'])->name('tickets.index');
+        Route::post('/store', [TicketController::class, 'store'])->name('tickets.store');
+        Route::put('/update/{id}', [TicketController::class, 'update'])->name('tickets.update');
+        Route::delete('/delete/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+        Route::post('/restore/{id}', [TicketController::class, 'restore'])->name('tickets.restore');
+        Route::post('/response/{id}', [TicketController::class, 'addResponse'])->name('tickets.addResponse');
+    });
+
+    // Rule Routes
+    Route::prefix('rules')->group(function () {
+        Route::post('/store', [RuleController::class, 'store'])->name('rules.store');
+        Route::put('/update/{id}', [RuleController::class, 'update'])->name('rules.update');
+        Route::delete('/delete/{id}', [RuleController::class, 'destroy'])->name('rules.destroy');
+        Route::post('/restore/{id}', [RuleController::class, 'restore'])->name('rules.restore');
+        Route::get('/index/{id?}', [RuleController::class, 'index'])->name('rules.index');
+    });
+
+    // Blog Routes
+    Route::prefix('blogs')->group(function () {
+        Route::post('/store', [BlogController::class, 'store'])->name('blogs.store');
+        Route::get('/show/{id}', [BlogController::class, 'show'])->name('blogs.show');
+        Route::put('/update/{id}', [BlogController::class, 'update'])->name('blogs.update');
+        Route::delete('/delete/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
+        Route::get('/restore/{id}', [BlogController::class, 'restore'])->name('blogs.restore');
+        Route::post('/upload/image/{id}', [BlogController::class, 'uploadImage'])->name('blogs.uploadImage');
+        Route::delete('/forcedelete/{id}', [BlogController::class, 'forcedelete'])->name('blogs.forceDelete');
+    });
+
+    // FAQ Routes
+    Route::prefix('faqs')->group(function () {
+        Route::post('/store', [FAQController::class, 'store'])->name('faqs.store');
+        Route::put('/update/{id}', [FAQController::class, 'update'])->name('faqs.update');
+        Route::delete('/delete/{id}', [FAQController::class, 'destroy'])->name('faqs.destroy');
+        Route::post('/restore/{id}', [FAQController::class, 'restore'])->name('faqs.restore');
+        Route::get('/index/{id?}', [FAQController::class, 'index'])->name('faqs.index');
+    });
+
+    // Comment Routes
+    Route::prefix('comments')->group(function () {
+        Route::post('/store', [CommentController::class, 'store'])->name('comments.store');
+        Route::put('/update/{id}', [CommentController::class, 'update'])->name('comments.update');
+        Route::delete('/delete/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
+        Route::post('/restore/{id}', [CommentController::class, 'restore'])->name('comments.restore');
+        Route::get('/show/{id}', [CommentController::class, 'show'])->name('comments.show');
+        Route::get('/index/{id?}', [CommentController::class, 'index'])->name('comments.index');
+    });
+
+    // Reservation Routes
+    Route::prefix('reservations')->group(function () {
+        Route::get('/index', [ReservationController::class, 'index'])->name('reservations.index');
+        Route::post('/store', [ReservationController::class, 'store'])->name('reservations.store');
+        Route::put('/update/{id}', [ReservationController::class, 'update'])->name('reservations.update');
+        Route::delete('/delete/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
+    });
+
+    // Passenger Routes
+    Route::prefix('passengers')->group(function () {
+        Route::get('/index', [PassengerController::class, 'index'])->name('passengers.index');
+        Route::post('/store', [PassengerController::class, 'store'])->name('passengers.store');
+        Route::put('/update/{id}', [PassengerController::class, 'update'])->name('passengers.update');
+        Route::delete('/delete/{id}', [PassengerController::class, 'destroy'])->name('passengers.destroy');
+    });
+
+    // Discount Code Routes
+    Route::prefix('discount_code')->group(function () {
+        Route::get('/index', [DiscountCodeController::class, 'index'])->name('discount_code.index');
+        Route::post('/store', [DiscountCodeController::class, 'store'])->name('discount_code.store');
+        Route::put('/update/{id}', [DiscountCodeController::class, 'update'])->name('discount_code.update');
+        Route::delete('/delete/{id}', [DiscountCodeController::class, 'destroy'])->name('discount_code.destroy');
+    });
+
+    // Emergency Contact Routes
+    Route::prefix('emergencies')->group(function () {
+        Route::get('/index', [EmergencyContactController::class, 'index'])->name('emergencies.index');
+        Route::post('/store', [EmergencyContactController::class, 'store'])->name('emergencies.store');
+        Route::put('/update/{id}', [EmergencyContactController::class, 'update'])->name('emergencies.update');
+        Route::delete('/delete/{id}', [EmergencyContactController::class, 'destroy'])->name('emergencies.destroy');
+    });
+
+    // Password Change
     Route::post('/password/change', [PasswordChangeController::class, 'update'])->name('password.update');
+
+    // Report Routes
+    Route::prefix('reports')->group(function () {
+        Route::get('/show/{productId}', [ReportController::class, 'show'])->name('reports.show');
+        Route::get('/index', [ReportController::class, 'allReports'])->name('reports.index');
+    });
+
+    // Ticket Routes
+    Route::prefix('ticketts')->group(function () {
+        Route::get('/store', [TickettController::class, 'create'])->name('ticketts.create');
+        Route::get('/index', [TickettController::class, 'index'])->name('ticketts.index');
+    });
 });
 
-//CommentRoute
-Route::middleware('auth:sanctum')->prefix('comments')->group(function () {
-    Route::get('/index{id?}', [CommentController::class, 'index'])->name('comments.index');
-    Route::post('/store', [CommentController::class, 'store'])->name('comments.store');
-    Route::put('/update/{id}', [CommentController::class, 'update'])->name('comments.update');
-    Route::delete('/delete/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
-    Route::post('/restore/{id}', [CommentController::class, 'restore'])->name('comments.restore');
-    Route::get('/show/{id}', [CommentController::class, 'show'])->name('comments.show');
-});
+// Routes for applying discount code
+Route::post('apply-discount-code', [ReservationController::class, 'applyDiscountCode'])->middleware('auth:sanctum')->name('apply.discount.code');
 
-//ReservationRoute
-Route::prefix('reservations')->middleware('auth:sanctum')->group(function () {
-    Route::get('/index', [ReservationController::class, 'index'])->name('reservation.index');
-    Route::post('/store', [ReservationController::class, 'store'])->name('reservation.store');
-    Route::put('/update/{id}', [ReservationController::class, 'update'])->name('reservation.update');
-    Route::delete('/delete/{id}', [ReservationController::class, 'destroy'])->name('Reservation.delete');
-});
-
-//user->show Prifile & Update Profile
+// Profile Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('profile', [UserController::class, 'profile']);
-    Route::get('showprofile', [UserController::class, 'showprofile']);
-});
-
-//PassengerRoute
-Route::prefix('passengers')->middleware('auth:sanctum')->group(function () {
-    Route::get('/index', [PassengerController::class, 'index'])->name('passengers.index');
-    Route::post('/store', [PassengerController::class, 'store'])->name('passengers.store');
-    Route::put('/update/{passengerId}', [PassengerController::class, 'update'])->name('passengers.update');
-    Route::delete('/delete/{id}', [PassengerController::class, 'destroy'])->name('passengers.delete');
-});
-
-// Routes for Discount Codes
-Route::middleware('auth:sanctum')->prefix('discount_code')->group(function () {
-    Route::get('index', [DiscountCodeController::class, 'index']);
-    Route::post('store', [DiscountCodeController::class, 'store']);
-   // Route::get('show/{id}', [DiscountCodeController::class, 'show']);
-    Route::put('update/{id}', [DiscountCodeController::class, 'update']);
-    Route::delete('delete/{id}', [DiscountCodeController::class, 'destroy']);
-});
-
-// Route for applying discount code
-Route::post('apply-discount-code', [ReservationController::class, 'aaplyDiscountCode'])->middleware('auth:sanctum');
-
-//EmrgencyContactRoute
-Route::prefix('emrgences')->middleware('auth:sanctum')->group(function () {
-    Route::get('/index', [EmergencyContactController::class, 'index'])->name('emrgences.index');
-    Route::post('/store', [EmergencyContactController::class, 'store'])->name('emrgences.store');
-    Route::put('/update/{id}', [EmergencyContactController::class, 'update'])->name('emrgences.update');
-    Route::delete('/delete/{id}', [EmergencyContactController::class, 'destroy'])->name('emrgences.destroy');
-});
-
-//Newsletter Route
-Route::middleware('auth:sanctum')->prefix('newslatter')->group(function () {
-    Route::post('/store', [NewsletterController::class, 'store']);
-    Route::get('/index', [NewsletterController::class, 'index']);
-});
-
-// Tickett Route
-Route::middleware('auth:sanctum')->prefix('ticketts')->group(function () {
-    Route::get('/store', [TickettController::class, 'create']);
-    Route::get('/index', [TickettController::class, 'index']);
-});
-
-
-Route::prefix('reports')->group(function(){
-Route::get('/show/{productId}', [ReportController::class, 'show']);
-Route::get('/index', [ReportController::class, 'allReports']);
+    Route::get('profile', [UserController::class, 'profile'])->name('user.profile');
+    Route::get('showprofile', [UserController::class, 'showprofile'])->name('user.showprofile');
 });
