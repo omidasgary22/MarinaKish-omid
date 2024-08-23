e927acaf998ab363277b974583c2e68ac451da86
Omid <omidasgary966@gmail.com>
2024-08-23 16:13:21 +0330

    add comment for method of RuleController & Optimizing it

--- app/Http/Controllers/RuleController.php
+++ app/Http/Controllers/RuleController.php
@@ -10,12 +10,18 @@ use Illuminate\Http\Request;
 
 class RuleController extends Controller
 {
+    /**
+     * List all rules.
+     */
     public function index()
     {
         $rules = Rule::all();
-        return response()->json(['rules' => 'rules']);
+        return response()->json(['rules' => $rules]);
     }
 
+    /**
+     * Create a new rule.
+     */
     public function store(StoreRuleReuest $request)
     {
         if ($request->user()->can('rule.store')) {
@@ -26,40 +32,52 @@ class RuleController extends Controller
         }
     }
 
+    /**
+     * Show a specific rule.
+     */
     public function show($id)
     {
-        $rule = Rule::findOrFill($id);
+        $rule = Rule::findOrFail($id);
         return response()->json(['rule' => $rule]);
     }
 
+    /**
+     * Update an existing rule.
+     */
     public function update(UpdateRuleRequest $request, $id)
     {
         if ($request->user()->can('rule.update')) {
             $rule = Rule::findOrFail($id);
             $rule->update($request->all());
-            return response()->json(['message' => 'قانون با موفیقیت به روز رسانی شد', 'rule' => $rule]);
+            return response()->json(['message' => 'قانون با موفقیت به روز رسانی شد', 'rule' => $rule]);
         } else {
             return response()->json(['message' => 'شما دسترسی لازم برای انجام این کار را ندارید'], 403);
         }
     }
 
+    /**
+     * Delete an existing rule.
+     */
     public function destroy(Request $request, $id)
     {
         if ($request->user()->can('rule.destroy')) {
             $rule = Rule::findOrFail($id);
-            $rule->delete($id);
+            $rule->delete();
             return response()->json(['message' => 'قانون با موفقیت حذف شد']);
         } else {
             return response()->json(['message' => 'شما دسترسی لازم برای انجام این کار را ندارید'], 403);
         }
     }
 
+    /**
+     * Restore a deleted rule.
+     */
     public function restore(Request $request, $id)
     {
         if ($request->user()->can('rule.restore')) {
-            $user = Rule::onlyTrashed()->findOrFail($id);
-            $user->restore();
-            return response()->json(['message' => 'قانون مورد نظر  با موفقیت بازیابی شد.'], 200);
+            $rule = Rule::onlyTrashed()->findOrFail($id);
+            $rule->restore();
+            return response()->json(['message' => 'قانون مورد نظر با موفقیت بازیابی شد.'], 200);
         } else {
             return response()->json(['message' => 'شما دسترسی لازم برای انجام این کار را ندارید'], 403);
         }
