c3c8d5ff63a947e0ddec68e3fc358f3d00b07a31
Omid <omidasgary966@gmail.com>
2024-08-23 14:06:04 +0330

    add comment for method of LogoutController & Optimizing it

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -7,10 +7,19 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
+    /**
+     * Handle the user logout process.
+     * This method revokes the current access token and logs the user out.
+     */
     public function logout(Request $request)
     {
+        // Revoke the current access token
         $request->user()->currentAccessToken()->delete();
+
+        // Log the user out of the web guard
         Auth::guard('web')->logout();
+
+        // Return a success message
         return response()->json(['message' => 'با موفقیت خارج شدید']);
     }
 }
