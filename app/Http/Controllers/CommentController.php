1562d296d5b9d88cb8ad0eaff54efb713999b861
Omid <omidasgary966@gmail.com>
2024-08-23 13:42:25 +0330

    add comment for method of CommentController & Optimizing it

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    /**
+     * Display a listing of the comments.
+     * This method returns a JSON response containing all comments with their related user and product data.
+     */
     public function index(Request $request)
     {
         if ($request->user()->can('comment.store')) {
@@ -19,26 +23,38 @@ class CommentController extends Controller
         }
     }
 
+    /**
+     * Store a newly created comment in storage.
+     * This method validates the request and creates a new comment if the user has the required permission.
+     */
     public function store(CommentRequest $request)
     {
         if ($request->user()->can('comment.store')) {
             $comment = Comment::create($request->toArray());
             return response()->json(['message' => 'نظر با موفقیت ایجاد شد', 'comment' => $comment], 201);
         } else {
-            return response()->json(['message' => 'شما دسترسی انجام این کا را ندرید'], 403);
+            return response()->json(['message' => 'شما دسترسی انجام این کا را ندارید'], 403);
         }
     }
 
+    /**
+     * Display the specified comment.
+     * This method returns a JSON response containing the comment details along with related user and product data.
+     */
     public function show(Request $request, $id)
     {
         if ($request->user()->can('comment.show')) {
             $comment = Comment::with('user', 'product')->findOrFail($id);
             return response()->json(['comment' => $comment]);
         } else {
-            return response()->json(['message' => 'شما دسترسی انجام این کار ار ندارید'], 403);
+            return response()->json(['message' => 'شما دسترسی انجام این کار را ندارید'], 403);
         }
     }
 
+    /**
+     * Update the specified comment in storage.
+     * This method validates the request and updates the comment if the user has the required permission.
+     */
     public function update(CommentRequest $request, $id)
     {
         if ($request->user()->can('comment.update')) {
@@ -50,6 +66,10 @@ class CommentController extends Controller
         }
     }
 
+    /**
+     * Remove the specified comment from storage.
+     * This method soft deletes the comment if the user has the required permission.
+     */
     public function destroy(Request $request, $id)
     {
         if ($request->user()->can('comment.destroy')) {
@@ -61,14 +81,18 @@ class CommentController extends Controller
         }
     }
 
+    /**
+     * Restore a soft deleted comment.
+     * This method restores the comment if the user has the required permission.
+     */
     public function restore(Request $request, $id)
     {
         if ($request->user()->can('comment.restore')) {
-            $user = Comment::onlyTrashed()->findOrFail($id);
-            $user->restore();
+            $comment = Comment::onlyTrashed()->findOrFail($id);
+            $comment->restore();
             return response()->json(['message' => 'نظر با موفقیت بازیابی شد.'], 200);
         } else {
-            return response()->json(['message' => 'شما دسترسی انجام این کا را دارید'], 403);
+            return response()->json(['message' => 'شما دسترسی انجام این کا را ندارید'], 403);
         }
     }
 }
