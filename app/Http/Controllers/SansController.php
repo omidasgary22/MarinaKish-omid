a0e52162951ddf8012646abea7ccaabe1bc63e53
Omid <omidasgary966@gmail.com>
2024-08-23 16:16:38 +0330

    add comment for method of SansController & Optimizing it

--- app/Http/Controllers/SansController.php
+++ app/Http/Controllers/SansController.php
@@ -7,7 +7,18 @@ use Illuminate\Http\Request;
 
 class SansController extends Controller
 {
-    public static function store($time, $pending, $total, $start_time, $ended_at, $id,$age_limit = 0)
+    /**
+     * Store new sessions for a product.
+     *
+     * @param int $time Duration of each session in minutes.
+     * @param int $pending Time pending between sessions in minutes.
+     * @param int $total Total capacity for each session.
+     * @param \Carbon\Carbon $start_time Start time of the first session.
+     * @param \Carbon\Carbon $ended_at End time for creating sessions.
+     * @param int $id Product ID.
+     * @param int $age_limit Age limit for the session.
+     */
+    public static function store($time, $pending, $total, $start_time, $ended_at, $id, $age_limit = 0)
     {
         while ($start_time->lessThan($ended_at)) {
             Sans::create([
@@ -20,6 +31,16 @@ class SansController extends Controller
         }
     }
 
+    /**
+     * Update sessions for a product by removing old ones and creating new ones.
+     *
+     * @param int $time Duration of each session in minutes.
+     * @param int $pending Time pending between sessions in minutes.
+     * @param int $total Total capacity for each session.
+     * @param \Carbon\Carbon $start_time Start time of the first session.
+     * @param int $id Product ID.
+     * @param \Carbon\Carbon $ended_at End time for creating sessions.
+     */
     public static function update($time, $pending, $total, $start_time, $id, $ended_at)
     {
         $sans = Sans::where('product_id', $id)->get();
