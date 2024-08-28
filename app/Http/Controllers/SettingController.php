46d3d4d4b92e0a7fedf63539f13b2a4a9e216e0d
Omid <omidasgary966@gmail.com>
2024-08-28 22:37:04 +0330

    add index and update method in setting controller

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function index()
+    {
+        $setting = Setting::all();
+        return response()->json(["setting" => $setting]);
+    }
+
+    public function update(Request $request)
+    {
+        $setting = Setting::where('key', $request->key)->first();
+
+        if ($setting) {
+            $setting->update($request->all);
+        } else {
+            $setting = Setting::create($request->all());
+        }
+
+        return response()->json($setting);
+    }
 }
